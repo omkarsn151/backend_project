534bf85b372b13fe71f907d778f7715bb31ee9ab
omkarsn151 <omkarsnimbalkar1501@gmail.com>
2025-01-09 13:36:42 +0530

    setpu configured

--- src/app.js
+++ src/app.js
@@ -3,9 +3,6 @@ import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import e from 'express';
 
-
-
-
 const app = express();
 
 app.use(cors({
@@ -18,5 +15,14 @@ app.use(express.json({limit: "16kb"}));
 app.use(urlencoded({extended: true, limit: "16kb"}));
 app.use(express.static("public"));
 app.use(cookieParser());
+
+
+// importing routes
+import userRoutes from './routes/user.routes.js';
+
+
+// routes declaration
+app.use("/api/v1/users", userRoutes);
+
 
 export default app;
--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,21 +1,27 @@
 const asyncHandler = (requestHandler) => {
-    (req, res, next) => {
-        return Promise.resolve(requestHandler(req, res, next)).catch((error) => next(error));
+    return (req, res, next) => {
+        Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err))
     }
 }
 
 
-// const asyncHandler = (requestHandler) => async (req, res, next) => {
-//     try {
-//         await requestHandler(req, res, next);
-//     }catch(error) { 
-//         res.status(error.code || 500).json({
-//             success: false,
-//             message: error.message || 'An error occurred',
-//              });
-//             }
-//         }
-
-
-
 export { asyncHandler }
+
+
+
+
+// const asyncHandler = () => {}
+// const asyncHandler = (func) => () => {}
+// const asyncHandler = (func) => async () => {}
+
+
+// const asyncHandler = (fn) => async (req, res, next) => {
+//     try {
+//         await fn(req, res, next)
+//     } catch (error) {
+//         res.status(err.code || 500).json({
+//             success: false,
+//             message: err.message
+//         })
+//     }
+// }
